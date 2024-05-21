b146ac4aabb39a538b58875e81b6c17812b9c573
Danish Fareed <104269116+danish-fareed@users.noreply.github.com>
2024-05-21 18:07:36 +0500

    Update create_task_for_batch.bat
    Now the script will check the IDM trial every 2 hours.

--- create_task_for_batch.bat
+++ create_task_for_batch.bat
@@ -1,15 +1,25 @@
 @echo off
 SET SCRIPT_PATH=%~dp0delete_registry_key.bat
-SET TASK_NAME=RunDeleteRegistryKey
+SET STARTUP_TASK_NAME=RunDeleteRegistryKeyAtStartup
+SET HOURLY_TASK_NAME=RunDeleteRegistryKeyHourly
 
-:: Check if the task already exists and delete it if it does
-schtasks /query /tn %TASK_NAME% >nul 2>&1
+:: Check if the startup task already exists and delete it if it does
+schtasks /query /tn %STARTUP_TASK_NAME% >nul 2>&1
 IF %ERRORLEVEL% == 0 (
-    schtasks /delete /tn %TASK_NAME% /f
+    schtasks /delete /tn %STARTUP_TASK_NAME% /f
 )
 
 :: Create a scheduled task to run the batch file at startup
-schtasks /create /tn %TASK_NAME% /tr "\"%SCRIPT_PATH%\"" /sc onstart /rl highest /f
+schtasks /create /tn %STARTUP_TASK_NAME% /tr "\"%SCRIPT_PATH%\"" /sc onstart /rl highest /f
 
-echo Task created successfully.
+:: Check if the hourly task already exists and delete it if it does
+schtasks /query /tn %HOURLY_TASK_NAME% >nul 2>&1
+IF %ERRORLEVEL% == 0 (
+    schtasks /delete /tn %HOURLY_TASK_NAME% /f
+)
+
+:: Create a scheduled task to run the batch file every 2 hours
+schtasks /create /tn %HOURLY_TASK_NAME% /tr "\"%SCRIPT_PATH%\"" /sc hourly /mo 2 /rl highest /f
+
+echo Tasks created successfully.
 pause
